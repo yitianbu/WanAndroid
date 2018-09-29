72b975e3daf364ec53ceb347f884bcba5edb7f7d
weifeng <weifeng@weidian.com>
2018-09-29 11:25:51 +0800

    加入WebViewActivity&优化FindFragment

--- app/src/main/java/com/weifeng/wanandroid/Adapter/ListArticlesAdapter.java
+++ app/src/main/java/com/weifeng/wanandroid/Adapter/ListArticlesAdapter.java
@@ -1,6 +1,7 @@
 package com.weifeng.wanandroid.Adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
@@ -17,6 +18,7 @@ import com.andview.refreshview.recyclerview.BaseRecyclerAdapter;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
+import com.weifeng.wanandroid.Activity.WebViewActivity;
 import com.weifeng.wanandroid.R;
 import com.weifeng.wanandroid.Utils.TimeUtil;
 import com.weifeng.wanandroid.model.ArticleContentItem;
@@ -50,8 +52,8 @@ public class ListArticlesAdapter extends BaseRecyclerAdapter<ListArticlesAdapter
     }
 
     @Override
-    public void onBindViewHolder(ArticleViewHolder holder, int position, boolean isItem) {
-        ArticleViewHolder articleHolder = (ArticleViewHolder) holder;
+    public void onBindViewHolder(ArticleViewHolder holder, final int position, boolean isItem) {
+        ArticleViewHolder articleHolder = holder;
         articleHolder.authorNameTv.setText(articleItems.get(position).author);
         articleHolder.publishTimeTv.setText(TimeUtil.getDate(articleItems.get(position).publishTime));
         articleHolder.chapterNameTv.setText(articleItems.get(position).chapterName + "/" + articleItems.get(position).superChapterName);
@@ -64,6 +66,15 @@ public class ListArticlesAdapter extends BaseRecyclerAdapter<ListArticlesAdapter
         }else {
             articleHolder.envelopePicImg.setVisibility(View.GONE);
         }
+        articleHolder.totalContentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, WebViewActivity.class);
+                intent.putExtra("url",articleItems.get(position).link);
+                context.startActivity(intent);
+            }
+        });
+
     }
 
 
@@ -85,6 +96,7 @@ public class ListArticlesAdapter extends BaseRecyclerAdapter<ListArticlesAdapter
         public TextView chapterNameTv;
         public TextView articleNameTv;
         public ImageView envelopePicImg;
+        public View totalContentView;
 
         public ArticleViewHolder(View itemView,boolean isItem) {
             super(itemView);
@@ -95,6 +107,7 @@ public class ListArticlesAdapter extends BaseRecyclerAdapter<ListArticlesAdapter
                 chapterNameTv = itemView.findViewById(R.id.tv_chapter_name);
                 articleNameTv = itemView.findViewById(R.id.tv_article_name);
                 envelopePicImg = itemView.findViewById(R.id.img_envelope_pic);
+                totalContentView = itemView;
             }
         }
     }
--- app/src/main/java/com/weifeng/wanandroid/Fragment/FindFragment.java
+++ app/src/main/java/com/weifeng/wanandroid/Fragment/FindFragment.java
@@ -1,5 +1,6 @@
 package com.weifeng.wanandroid.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -17,6 +18,7 @@ import com.andview.refreshview.XRefreshViewFooter;
 import com.bigkoo.convenientbanner.ConvenientBanner;
 import com.bigkoo.convenientbanner.holder.CBViewHolderCreator;
 import com.bigkoo.convenientbanner.listener.OnItemClickListener;
+import com.weifeng.wanandroid.Activity.WebViewActivity;
 import com.weifeng.wanandroid.Adapter.ListArticlesAdapter;
 import com.weifeng.wanandroid.Holder.LocalImageHolderView;
 import com.weifeng.wanandroid.model.ArticleHeadItem;
@@ -43,6 +45,8 @@ public class FindFragment extends Fragment implements OnItemClickListener {
     ConvenientBanner convenientBanner;
     private int page = 0;
 
+    List<ArticleHeadItem> articleHeadItemList;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -53,7 +57,9 @@ public class FindFragment extends Fragment implements OnItemClickListener {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        initView(view);
+        if(this.rootView == null) {
+            initView(view);
+        }
         loadHeadData();
         loadContentData();
     }
@@ -63,7 +69,7 @@ public class FindFragment extends Fragment implements OnItemClickListener {
             @Override
             public void onResponse(Call<ArticlesHeadResponse> call, Response<ArticlesHeadResponse> response) {
                 if (200 == response.code()) {
-                    List<ArticleHeadItem> articleHeadItemList = response.body().getData();
+                    articleHeadItemList = response.body().getData();
                     List<String> imgUrls = new ArrayList<>();
                     for (int i = 0; i < articleHeadItemList.size(); i++) {
                         imgUrls.add(articleHeadItemList.get(i).getImagePath());
@@ -90,11 +96,14 @@ public class FindFragment extends Fragment implements OnItemClickListener {
                     listArticlesAdapter.setArticleItems(listArticlesResponse.data.datas);
                 }
                 xRefreshView.stopLoadMore();
+                xRefreshView.stopRefresh();
             }
 
             @Override
             public void onFailure(Call<ListArticlesResponse> call, Throwable t) {
                 Log.e("", "");
+                xRefreshView.stopLoadMore();
+                xRefreshView.stopRefresh();
             }
         });
     }
@@ -102,7 +111,7 @@ public class FindFragment extends Fragment implements OnItemClickListener {
     private void initView(View view) {
         this.rootView = view;
         xRefreshView = view.findViewById(R.id.content_xRefreshView);
-        xRefreshView.setPullRefreshEnable(false);    //禁止下拉刷新
+        xRefreshView.setPullRefreshEnable(true);    //禁止下拉刷新
         xRefreshView.setPullLoadEnable(true);
         xRefreshView.setAutoLoadMore(true);
         xRefreshView.enableRecyclerViewPullUp(true);
@@ -115,7 +124,8 @@ public class FindFragment extends Fragment implements OnItemClickListener {
 
             @Override
             public void onRefresh(boolean isPullDown) {
-
+                loadHeadData();
+                loadContentData();
             }
 
             @Override
@@ -136,15 +146,14 @@ public class FindFragment extends Fragment implements OnItemClickListener {
         contentRV = view.findViewById(R.id.content_rv);
         listArticlesAdapter = new ListArticlesAdapter(rootView.getContext());
         listArticlesAdapter.setCustomLoadMoreView(new XRefreshViewFooter(view.getContext()));
-        initHeader();
-        listArticlesAdapter.setHeaderView(convenientBanner, contentRV);
+        listArticlesAdapter.setHeaderView(getHeadView(), contentRV);
         contentRV.setAdapter(listArticlesAdapter);
         contentRV.addItemDecoration(new DividerItemDecoration(rootView.getContext(), DividerItemDecoration.VERTICAL));
         contentRV.setLayoutManager(new LinearLayoutManager(rootView.getContext()));
     }
 
 
-    private View initHeader() {
+    private View getHeadView() {
         View header = LayoutInflater.from(rootView.getContext()).inflate(R.layout.ll_article_head, null);
         convenientBanner = header.findViewById(R.id.convenientBanner);
         //本地图片例子
@@ -176,6 +185,8 @@ public class FindFragment extends Fragment implements OnItemClickListener {
 
     @Override
     public void onItemClick(int position) {
-
+        Intent intent = new Intent(this.getActivity(), WebViewActivity.class);
+        intent.putExtra("url",articleHeadItemList.get(position).getUrl());
+        startActivity(intent);
     }
 }
